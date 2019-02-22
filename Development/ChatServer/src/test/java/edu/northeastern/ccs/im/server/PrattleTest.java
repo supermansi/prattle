f72a9a31332585bde0ccb1589e311b6f4975c4f9
Rohan Gori <gori.r@husky.neu.edu>
2019-02-22 15:08:33 -0500

    Added tests for prattle

--- Development/ChatServer/src/test/java/edu/northeastern/ccs/im/server/PrattleTest.java
+++ Development/ChatServer/src/test/java/edu/northeastern/ccs/im/server/PrattleTest.java
@@ -1,18 +1,31 @@
 package edu.northeastern.ccs.im.server;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import edu.northeastern.ccs.im.Message;
 import edu.northeastern.ccs.im.NetworkConnection;
 import java.io.IOException;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.net.SocketOption;
+import java.nio.ByteBuffer;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.nio.channels.spi.SelectorProvider;
+import java.util.Set;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -46,7 +59,7 @@ public class PrattleTest {
     serverSocketChannel.socket().bind(new InetSocketAddress(PORT));
     SocketChannel clientSocket = SocketChannel.open();
     networkConnection = new NetworkConnection(clientSocket);
-    Mockito.when(clientRunnable.isInitialized()).thenReturn(true);
+    when(clientRunnable.isInitialized()).thenReturn(true);
 
     Class clazz = Prattle.class;
     Field field = clazz.getDeclaredField("active");
@@ -71,7 +84,7 @@ public class PrattleTest {
 
   @Test
   public void testBroadcastMessageFalse() throws IOException {
-    Mockito.when(clientRunnable.isInitialized()).thenReturn(false);
+    when(clientRunnable.isInitialized()).thenReturn(false);
     Message message = Message.makeBroadcastMessage("abcd", "hello world");
     Prattle.broadcastMessage(message);
     serverSocketChannel.close();
@@ -103,5 +116,87 @@ public class PrattleTest {
       String[] args = new String[0];
       Prattle.main(args);
     }
+  }
+
+  @Test
+  public void testCreateClientThread() throws IllegalAccessException, InvocationTargetException, InstantiationException, IOException {
+    Class<Prattle> clazz = Prattle.class;
+    Method method[] = clazz.getDeclaredMethods();
+    Method met = null;
+    for (Method m : method) {
+      if (m.getName().contains("createClientThread")) {
+        met = m;
+      }
+    }
+    met.setAccessible(true);
+    ScheduledExecutorService executor = mock(ScheduledExecutorService.class);
+    ScheduledFuture scheduledFuture = mock(ScheduledFuture.class);
+    when(executor.scheduleAtFixedRate(clientRunnable,CLIENT_CHECK_DELAY,CLIENT_CHECK_DELAY, TimeUnit.MILLISECONDS)).thenReturn(scheduledFuture);
+    met.invoke(null,serverSocketChannel,executor);
+    serverSocketChannel.close();
+  }
+
+  @Test
+  public void testCreateClientThreadNotNullSock() throws IllegalAccessException, InvocationTargetException, InstantiationException, IOException {
+    ServerSocketChannel serverSocketChannel = mock(ServerSocketChannel.class);
+    SocketChannel sockChan = mock(SocketChannel.class);
+    when(serverSocketChannel.accept()).thenReturn(sockChan);
+    Class<Prattle> clazz = Prattle.class;
+    Method method[] = clazz.getDeclaredMethods();
+    Method met = null;
+    for (Method m : method) {
+      if (m.getName().contains("createClientThread")) {
+        met = m;
+      }
+    }
+    met.setAccessible(true);
+    ScheduledExecutorService executor = mock(ScheduledExecutorService.class);
+    ScheduledFuture scheduledFuture = mock(ScheduledFuture.class);
+    when(executor.scheduleAtFixedRate(clientRunnable,CLIENT_CHECK_DELAY,CLIENT_CHECK_DELAY, TimeUnit.MILLISECONDS)).thenReturn(scheduledFuture);
+    met.invoke(null,serverSocketChannel,executor);
+    this.serverSocketChannel.close();
+  }
+
+  @Test
+  public void testCreateClientThreadNotNullSockAssertExcept() throws IllegalAccessException, InvocationTargetException, InstantiationException, IOException {
+    ServerSocketChannel serverSocketChannel = mock(ServerSocketChannel.class);
+    SocketChannel sockChan = mock(SocketChannel.class);
+    doThrow(new IOException()).when(sockChan).configureBlocking(false);
+    when(serverSocketChannel.accept()).thenReturn(sockChan);
+    Class<Prattle> clazz = Prattle.class;
+    Method method[] = clazz.getDeclaredMethods();
+    Method met = null;
+    for (Method m : method) {
+      if (m.getName().contains("createClientThread")) {
+        met = m;
+      }
+    }
+    met.setAccessible(true);
+    ScheduledExecutorService executor = mock(ScheduledExecutorService.class);
+    ScheduledFuture scheduledFuture = mock(ScheduledFuture.class);
+    when(executor.scheduleAtFixedRate(clientRunnable,CLIENT_CHECK_DELAY,CLIENT_CHECK_DELAY, TimeUnit.MILLISECONDS)).thenReturn(scheduledFuture);
+    met.invoke(null,serverSocketChannel,executor);
+    this.serverSocketChannel.close();
+  }
+
+  @Test
+  public void testCreateClientThreadNotNullSockIOExcept() throws IllegalAccessException, InvocationTargetException, InstantiationException, IOException {
+    ServerSocketChannel serverSocketChannel = mock(ServerSocketChannel.class);
+    SocketChannel sockChan = mock(SocketChannel.class);
+    doThrow(new IOException()).when(serverSocketChannel).accept();
+    Class<Prattle> clazz = Prattle.class;
+    Method method[] = clazz.getDeclaredMethods();
+    Method met = null;
+    for (Method m : method) {
+      if (m.getName().contains("createClientThread")) {
+        met = m;
+      }
+    }
+    met.setAccessible(true);
+    ScheduledExecutorService executor = mock(ScheduledExecutorService.class);
+    ScheduledFuture scheduledFuture = mock(ScheduledFuture.class);
+    when(executor.scheduleAtFixedRate(clientRunnable,CLIENT_CHECK_DELAY,CLIENT_CHECK_DELAY, TimeUnit.MILLISECONDS)).thenReturn(scheduledFuture);
+    met.invoke(null,serverSocketChannel,executor);
+    this.serverSocketChannel.close();
   }
 }
