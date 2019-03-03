32eb017d4856d96e7d1cddf3a8ef37745034f06c
Aditi Kacheria <kacheria.a@husky.neu.edu>
2019-03-02 19:45:41 -0500

    Committing DAOs

--- Development/ChatServer/src/main/java/edu/northeastern/ccs/im/model/User.java
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/model/User.java
@@ -1,46 +1,69 @@
 package edu.northeastern.ccs.im.model;
 
 public class User {
-	
-	private int userID;
-	private String userFN;
-	private String userLN;
-	private String email;
-	private String password;
-	
-	public int getUserID() {
-		return userID;
-	}
-	public void setUserID(int userID) {
-		this.userID = userID;
-	}
-	
-	public String getUserFN() {
-		return userFN;
-	}
-	public void setUserFN(String userFN) {
-		this.userFN = userFN;
-	}
-	
-	public String getUserLN() {
-		return userLN;
-	}
-	public void setUserLN(String userLN) {
-		this.userLN = userLN;
-	}
-	
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
+
+  private int userID;
+  private String username;
+  private String userFN;
+  private String userLN;
+  private String email;
+  private String password;
+
+  public User(String username, String userFN, String userLN, String email, String password) {
+    this.username = username;
+    this.userFN = userFN;
+    this.userLN = userLN;
+    this.email = email;
+    this.password = password;
+  }
+
+  public int getUserID() {
+    return userID;
+  }
+
+  public void setUserID(int userID) {
+    this.userID = userID;
+  }
+
+  public String getUserFN() {
+    return userFN;
+  }
+
+  public void setUserFN(String userFN) {
+    this.userFN = userFN;
+  }
+
+  public String getUserLN() {
+    return userLN;
+  }
+
+  public void setUserLN(String userLN) {
+    this.userLN = userLN;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
 }
