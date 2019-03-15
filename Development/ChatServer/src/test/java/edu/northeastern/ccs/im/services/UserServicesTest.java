3036f415d46126d3a9a1555fcc5f306236a4cce6
Aditi Kacheria <kacheria.a@husky.neu.edu>
2019-03-15 14:02:29 -0400

    Committing UserServices tests

--- Development/ChatServer/src/test/java/edu/northeastern/ccs/im/services/UserServicesTest.java
+++ Development/ChatServer/src/test/java/edu/northeastern/ccs/im/services/UserServicesTest.java
@@ -13,11 +13,13 @@ public class UserServicesTest {
 
   UserDAO userDAO;
   User user;
+  User user1;
 
   @Before
   public void setUp() {
     userDAO = UserDAO.getInstance();
-    user = new User("Adi","Adi","K", "adi@gmail.com","1234");
+    user = new User("Aditi","Aditi","Kacheria", "aditik@gmail.com","12345");
+    user1 = new User("Daba","Daba","Daba", "daba@gmail.com","daba");
   }
 
   @After
@@ -28,9 +30,77 @@ public class UserServicesTest {
   }
 
   @Test
-  public void testLogin() {
+  public void testLoginSuccess() {
     userDAO.createUser(user);
     assertEquals(true, UserServices.login(user.getUsername(),user.getPassword()));
+  }
 
+  @Test
+  public void testLoginFailure() {
+    userDAO.createUser(user);
+    assertEquals(false, UserServices.login(user.getUsername(),user1.getPassword()));
+  }
+
+  @Test
+  public void testRegistrationSuccess() {
+    assertEquals(true,UserServices.register(user.getUsername(),user.getUserFN(),user.getUserLN(),user.getUserLN(),user.getPassword()));
+  }
+
+  @Test
+  public void testRegistrationFailure() {
+    UserServices.register(user.getUsername(),user.getUserFN(),user.getUserLN(),user.getUserLN(),user.getPassword());
+    assertEquals(false,UserServices.register(user.getUsername(),user.getUserFN(),user.getUserLN(),user.getUserLN(),user.getPassword()));
+  }
+
+  @Test
+  public void testDeleteUserSuccess() {
+    userDAO.createUser(user);
+    assertEquals(true, userDAO.isUserExists(user.getUsername()));
+    UserServices.deleteUser(user.getUsername());
+    assertEquals(false,userDAO.isUserExists(user.getUsername()));
+  }
+
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testDeleteUserFailure() {
+    UserServices.deleteUser(user.getUsername());
+    assertEquals(false,userDAO.isUserExists(user.getUsername()));
+  }
+
+  @Test
+  public void testUpdateFN() {
+    userDAO.createUser(user);
+    UserServices.updateFN(user.getUsername(),"Abibi");
+    assertEquals("Abibi",userDAO.getUserByUsername(user.getUsername()).getUserFN());
+  }
+
+  @Test
+  public void testUpdateLN() {
+    userDAO.createUser(user);
+    UserServices.updateLN(user.getUsername(),"Kach");
+    assertEquals("Kach",userDAO.getUserByUsername(user.getUsername()).getUserLN());
+  }
+
+
+  @Test
+  public void testUpdateEmail() {
+    userDAO.createUser(user);
+    UserServices.updateEmail(user.getUsername(),"kach@gmail.com");
+    assertEquals("kach@gmail.com",userDAO.getUserByUsername(user.getUsername()).getEmail());
+  }
+
+  @Test
+  public void testUpdatePassword() {
+    userDAO.createUser(user);
+    UserServices.updatePassword(user.getUsername(),"password");
+    assertEquals("password",userDAO.getUserByUsername(user.getUsername()).getPassword());
+  }
+
+  @Test
+  public void testUpdateLastSeen() {
+    userDAO.createUser(user);
+    long time = System.currentTimeMillis();
+    UserServices.updateLastSeen(user.getUsername(),time);
+    assertEquals(Long.toString(time), userDAO.getUserByUsername(user.getUsername()).getLastSeen());
   }
 }
