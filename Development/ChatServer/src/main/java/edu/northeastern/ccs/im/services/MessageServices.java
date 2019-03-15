97b998af5953b39c9d9f32f1169ddb0e081b13ce
Mansi Jain <mansijain@ccs.neu.edu>
2019-03-15 04:06:10 -0400

    fixed services

--- Development/ChatServer/src/main/java/edu/northeastern/ccs/im/services/MessageServices.java
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/services/MessageServices.java
@@ -3,7 +3,6 @@ package edu.northeastern.ccs.im.services;
 import java.util.List;
 
 import edu.northeastern.ccs.im.dao.GroupDAO;
-import edu.northeastern.ccs.im.dao.GroupToUserDAO;
 import edu.northeastern.ccs.im.dao.MessageDAO;
 import edu.northeastern.ccs.im.dao.MessageToUserDAO;
 import edu.northeastern.ccs.im.dao.UserDAO;
@@ -13,7 +12,6 @@ import edu.northeastern.ccs.im.model.Message;
 public class MessageServices {
 	
 	private static GroupDAO groupDAO;
-	private static GroupToUserDAO groupUserDAO;
 	private static UserDAO userDAO;
 	private static MessageDAO messageDAO;
 	private static MessageToUserDAO messageUserDAO;
@@ -24,34 +22,33 @@ public class MessageServices {
 	
 	static {
 		groupDAO = GroupDAO.getInstance();
-		groupUserDAO = GroupToUserDAO.getInstance();
 		userDAO = UserDAO.getInstance();
 		messageDAO = MessageDAO.getInstance();
 		messageUserDAO = MessageToUserDAO.getInstance();
 	}
 	
 	public static boolean addMessage(Message.MsgType msgType, String sender, String receiver, String message) {
-		boolean isReceiverValid = false;
 		if(msgType == Message.MsgType.PVT) {
 		     if(userDAO.isUserExists(receiver)) {
-		       isReceiverValid = true;
+		    	int senderID = userDAO.getUserByUsername(sender).getUserID();
+				Message sendMessage = new Message(msgType, senderID, message, Long.toString(System.currentTimeMillis()));
+				messageDAO.createMessage(sendMessage);
+				messageUserDAO.mapMsgIdToReceiverId(sendMessage, userDAO.getUserByUsername(receiver).getUserID());
+				return true;
 		     }
 		   }
 		   else if(msgType == Message.MsgType.GRP) {
 		     if(groupDAO.checkGroupExists(receiver)) {
-		       isReceiverValid = true;
+		    	int senderID = userDAO.getUserByUsername(sender).getUserID();
+				Message sendMessage = new Message(msgType, senderID, message, Long.toString(System.currentTimeMillis()));
+				messageDAO.createMessage(sendMessage);
+				messageUserDAO.mapMsgIdToReceiverId(sendMessage, groupDAO.getGroupByGroupName(receiver).getGrpID());
+				return true;
 		     }
 		   }
 		   else {
 		     throw new DatabaseConnectionException("This is not a valid handle.");
 		   }
-		if(isReceiverValid) {
-			int senderID = userDAO.getUserByUsername(sender).getUserID();
-			Message sendMessage = new Message(msgType, senderID, message, Long.toString(System.currentTimeMillis()));
-			messageDAO.createMessage(sendMessage);
-			messageUserDAO.mapMsgIdToReceiverId(sendMessage, userDAO.getUserByUsername(receiver).getUserID());
-			return true;
-		}
 		return false;
 	}
 
