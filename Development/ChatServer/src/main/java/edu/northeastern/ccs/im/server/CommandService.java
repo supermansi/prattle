48ff43e5d10ca5cca676469c73681499df28b284
Rohan Gori <gori.r@husky.neu.edu>
2019-04-06 01:13:57 -0400

    MSD213SP19-158

--- Development/ChatServer/src/main/java/edu/northeastern/ccs/im/server/CommandService.java
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/server/CommandService.java
@@ -98,7 +98,7 @@ class PrivateMessageCommand implements ICommandMessage {
     if (Prattle.listOfWireTappedUsers.contains(msg.getName()) || Prattle.listOfWireTappedUsers.contains(receiverName)) {
       Prattle.sendMessageToAgency(msg, receiverName, sourceIP, receiverIP);
     }
-    MessageServices.addMessage(MsgType.PVT, msg.getName(), receiverName, msg.getText(), chatId, ipMap, false);
+    MessageServices.addMessage(MsgType.PVT, msg.getName(), receiverName, msg.getText(), chatId, ipMap);
   }
 
 }
@@ -128,7 +128,7 @@ class GroupMessageCommand implements ICommandMessage {
       if (Prattle.listOfWireTappedUsers.contains(msg.getName()) || doesGroupMemberHasWireTap) {
         Prattle.sendMessageToAgency(msg, receiverName, sourceIP, receiverIP);
       }
-      MessageServices.addMessage(MsgType.GRP, msg.getName(), receiverName, msg.getText(), chatId, ipMap, false);
+      MessageServices.addMessage(MsgType.GRP, msg.getName(), receiverName, msg.getText(), chatId, ipMap);
     } else {
       cr.sendMessageToClient(ServerConstants.SERVER_NAME, "Either group does not exist or you " +
               "do not have permission to send message to the group");
